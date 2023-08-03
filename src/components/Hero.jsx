2713915d347fc11185250e73c9ce1c8997e73f95
YoungDevelopment <youngstunner2000@gmail.com>
2023-08-03 09:44:49 -0500

    forth commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,13 @@ export default function Hero() {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"></div>
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText}`}>
+            Hi, I'm <span className="text-[#915eff] ">Abdul Rehman</span>
+          </h1>
         </div>
       </div>
     </section>
