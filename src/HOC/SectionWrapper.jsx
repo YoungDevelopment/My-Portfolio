72ec8df2d20fe957958ca0d524eb4fabb0afc1de
YoungDevelopment <youngstunner2000@gmail.com>
2023-08-07 17:59:48 -0500

    second commit 08-07-2023

--- src/HOC/SectionWrapper.jsx
+++ src/HOC/SectionWrapper.jsx
@@ -1,11 +1,25 @@
 import { motion } from "framer-motion";
 
-export default function SectionWrapper(Component, idName) {
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
-      <motion.section>
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+
         <Component />
       </motion.section>
     );
-  }
-}
+  };
+
+export default StarWrapper;
